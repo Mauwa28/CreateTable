33b3afe42723f27dd9a83ea46cc9ba973706640c
Mauwa28 <116839503+Mauwa28@users.noreply.github.com>
2023-06-04 17:15:17 -0700

    Add files via upload

---
+++ SQLQuery_CreateTable.sql
@@ -0,0 +1,24 @@
+--CREATE TABLE student
+--(Id int, 
+--First_name varchar(32),
+--Last_name varchar (32),
+--Address varchar(32),
+--Postal_code varchar(32),
+--Phone_number varchar(32),
+--Age smallint);
+
+-----Let's verify the table 
+--SELECT*
+--FROM student
+-----Done, works great!
+
+--INSERT INTO student VALUES
+--(1, 'Adam', 'Smith', 'Los Angeles', '90025', '3102593641', 15),
+--(2, 'Anna', 'Lowes', 'Westminster', '90041', '3108749636', 16),
+--(3, 'Linda', 'Klo', 'Santa monica', '90452', '3108527496', 15),
+--(4, 'Douglas', 'Williams', 'Laguna beach', '90758', '3108521595', 16),
+--(5, 'Mike', 'Jackson', 'Long beach', '90458', '2148527496', 17);
+
+-----Let's verify our new table
+--SELECT*
+--FROM student
